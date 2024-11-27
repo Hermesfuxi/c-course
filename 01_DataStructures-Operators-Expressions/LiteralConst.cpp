f73a97fc6e4256e2a82bd7f91fcd135b4df3a621
hermesfuxi <tang2472854207@outlook.com>
2024-11-27 15:09:47 +0800

    add: c++ project demo test

---
+++ 01_DataStructures-Operators-Expressions/LiteralConst.cpp
@@ -0,0 +1,11 @@
+//
+// Created by hermesfuxi on 2024/11/26 0026.
+//
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    cout << "11111" << endl;
+    return 0;
+};
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * 主函数
+ *
+ * 该函数是程序的入口点它输出一条消息到控制台
+ * 没有参数传递给这个函数，也不返回任何值
+ */
+int main()
+{
+    // 输出欢迎信息到控制台
+    std::cout<<"Hello, World!"<<std::endl;
+    return 0;
+}
+
